8abb86d62c23dd2583cec192918621002643dac8
NouraAzab <noura.azab1357@gmail.com>
2025-12-24 13:49:41 +0200

    create [markCompleted] function

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
 const formElement = document.querySelector("form");
 const inputElement = document.querySelector("input");
 const apiKey = "6949582f600ccb137ffe54be";
-let allTODOs ;
+let allTODOs;
 getAllTODOs();//contains displayAllTODOs()  else
 
 
@@ -114,9 +114,9 @@ function displayAllTODOs() {
         cartona += `
 
         <li class="d-flex justify-content-between align-items-center border-bottom pb-2 my-2">
-                <span class="task-name" style="${todoElement.completed ? `text-decoration: line-through;`: ``}">${todoElement.title}</span>
+                <span onclick="markCompleted('${todoElement._id}');" class="task-name ${todoElement.completed? `completed`: ``}" style="${todoElement.completed ? `text-decoration: line-through;` : ``}">${todoElement.title}</span>
                 <div class="d-flex align-items-center gap-4 ">
-                    <span><i class="fa-solid fa-circle-check ${todoElement.completed? `d-block` : `d-none`  }" style="color: #63E6BE;"></i></span>
+                    <span><i class="fa-solid fa-circle-check ${todoElement.completed ? `d-block` : `d-none`}" style="color: #63E6BE;"></i></span>
                     <span class="trash-icon-wrapper rounded"><i class="fa-solid fa-trash text-white "></i></span>
                 </div>
             </li>
@@ -126,7 +126,41 @@ function displayAllTODOs() {
 
     }
     document.getElementById("rowData").innerHTML = cartona;
-   
+
 }
 // ====================================
+
+async function markCompleted(todoID) {
+
+    const todoData = {
+        todoId: todoID
+    }
+    const obj = {
+            method:"put", // put : for update
+            body:JSON.stringify(todoData),
+            headers:{
+                "content-type":"application/json"
+            }
+        }
+
+
+
+    const response = await fetch("https://todos.routemisr.com/api/v1/todos", obj);
+    // console.log(response);
+    if(response.ok){
+        const data = await response.json();
+        // console.log(data);
+        if(data.message === "success"){// in backend ->the todo become {... , completed = true , ...}
+            //will not call displayAllTODOs() ...because it use allTODOs array which is empty after loading ^^
+            //so will use getAllTODOs.. contain displayAllTODOs and update the array with the data from the API ^^
+
+           console.log( document.querySelector(".TODO li .task-name:hover:after"));
+            getAllTODOs();
+        
+
+        }
+    }
+    
+
+}
 
